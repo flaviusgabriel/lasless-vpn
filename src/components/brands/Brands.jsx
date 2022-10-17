8ee6b956b54f119d87f9aafd4bd2617226d60e28
flaviusgabriel <62425734+flaviusgabriel@users.noreply.github.com>
2022-10-17 16:54:04 +0300

    brands section align

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -11,7 +11,7 @@ const Brands = () => {
 
   return (
     <div>
-      <div class="container d-flex align-items-center justify-content-center">
+      <div class=" container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4 py-5 align-items-center justify-content-center">
           {names.map((item, index) => {
             return (
