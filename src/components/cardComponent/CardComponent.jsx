1e0f3230059fa4ea7c2e514ddb002bc84ddee4db
flaviusgabriel <62425734+flaviusgabriel@users.noreply.github.com>
2022-10-19 18:01:14 +0300

    align carousel buttons and dots

--- src/components/cardComponent/CardComponent.jsx
+++ src/components/cardComponent/CardComponent.jsx
@@ -4,12 +4,8 @@ import profileImg from "../../assets/img/Profile.jpg";
 const CardComponent = (props) => {
   return (
     <div>
-      <div class="card h-100 p-5  rounded-3 border border-danger testimonial-card card-body flex-fill">
-        <a
-          href="#"
-          class="list-group-item list-group-item-action d-flex  gap-3 py-3 border-0 "
-          aria-current="true"
-        >
+      <div class="card h-100 p-5  rounded-3 border testimonial-card card-body flex-fill">
+        <a class="list-group-item list-group-item-action d-flex  gap-3 py-3 border-0 ">
           <img
             src={props.linkImg}
             alt="twbs"
@@ -22,7 +18,8 @@ const CardComponent = (props) => {
               <h6 class="mb-0">{props.name}</h6>
               <span class="mb-0 opacity-75">{props.location}</span>
             </div>
-            <small class="text-muted">
+            <div className=""></div>
+            <small class="text-muted d-flex justify-content-center align-items-center rate">
               {props.rate}
               <ion-icon name="star-sharp"></ion-icon>
             </small>
--- src/components/commentsCarousel/CommentsCarousel.jsx
+++ src/components/commentsCarousel/CommentsCarousel.jsx
@@ -12,108 +12,22 @@ import { testimonials } from "./Datas";
 
 import imgGirl from "../../assets/img/Profile.jpg";
 import { dataDigitalBestSeller } from "./Data";
+import { useRef } from "react";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "red" }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "green" }}
-      onClick={onClick}
-    />
-  );
-}
+import styles from "./comments-carousel.scss";
 
 const CommentsCarousel = () => {
-  // const testimonials = [
-  //   {
-  //     name: "Viezh Robert",
-  //     location: "Warsaw, Poland",
-  //     rate: "4.5",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/9.jpg",
-  //     comment:
-  //       "“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
-  //   },
-
-  //   {
-  //     name: "Yessica Christy",
-  //     location: "Shanxi, China",
-  //     rate: "4.5",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/2.jpg",
-  //     comment:
-  //       "“I like it because I like to travel far and still can connect with high speed.”.",
-  //   },
-  //   {
-  //     name: "Kim Young Jou",
-  //     location: "Seoul, South Korea",
-  //     rate: "4.5",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/1.jpg",
-  //     comment:
-  //       "“This is very unusual for my business that currently requires a virtual private network that has high security.”.",
-  //   },
-  //   {
-  //     name: "Lance Jarvis",
-  //     location: "London, UK",
-  //     rate: "4.8",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/3.jpg",
-  //     comment:
-  //       "“We have been using product for last one year, and I have to say that it has transformed the way we do business. Thank you for awesome service.”.",
-  //   },
-  //   {
-  //     name: "Kylie Lutz",
-  //     location: "Paris, France",
-  //     rate: "5",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/5.jpg",
-  //     comment:
-  //       "“The company really helped with my personal branding - everything from conception to execution was amazing and super professional. Looking forwards for upgradation.”.",
-  //   },
-  //   {
-  //     name: "Diane Smith",
-  //     location: "New York, USA",
-  //     rate: "4.1",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/4.jpg",
-  //     comment:
-  //       "“Team offers a powerful suite of tools that every client must have. And if you get stuck their support team will help out. Its is fast, reliable and has no complications at all.”.",
-  //   },
-  //   {
-  //     name: "Mikayla Mccullough",
-  //     location: "Madrid, Spain",
-  //     rate: "4.9",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/10.jpg",
-  //     comment:
-  //       "“The vibe, level of inquiry, feedback and traffic at our product services and newly designed booth at a recent conference was very positive.”.",
-  //   },
-  //   {
-  //     name: "Kamren Hays",
-  //     location: "Florida, USA",
-  //     rate: "4.1",
-  //     linkImg: "https://testimonialapi.toolcarton.com/avatar/8.jpg",
-  //     comment:
-  //       "“Product bring 10x multiplier in terms of both ease and spread of use. I'm able to quickly and easily execute on my marketing strategies.”.",
-  //   },
-  // ];
-
   const [defaultImage, setDefaultImage] = useState({});
   const settings = {
     dots: true,
-    infinite: false,
+    dotsClass: "button__bar",
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
     initialSlide: 0,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
+    // dotsClass: "button__bar",
+    // nextArrow: <SampleNextArrow />,
+    // prevArrow: <SamplePrevArrow />,
     responsive: [
       {
         breakpoint: 1024,
@@ -142,14 +56,33 @@ const CommentsCarousel = () => {
     ],
   };
 
-  const handleErrorImage = (data) => {
-    setDefaultImage((prev) => ({
-      ...prev,
-      [data.target.alt]: data.target.alt,
-      linkDefault: imgGirl,
-    }));
+  // Next - Prev buttons
+  const customeSlider = useRef();
+
+  const next = () => {
+    customeSlider.current.slickNext();
   };
 
+  const previous = () => {
+    customeSlider.current.slickPrev();
+  };
+
+  const settingsNoModules = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dotsClass: "button__bar",
+    arrows: false,
+  };
+
+  const settingsWithModules = {
+    ...settings,
+    dotsClass: styles.button__bar,
+  };
   return (
     <div class="comments">
       <div class="container card-carousel">
@@ -169,7 +102,7 @@ const CommentsCarousel = () => {
 
         {/* Carousel */}
         <div className="py-5 container">
-          <Slider {...settings}>
+          <Slider {...settings} ref={customeSlider}>
             {testimonials.map((item, index) => {
               return (
                 <div key={index}>
@@ -186,6 +119,33 @@ const CommentsCarousel = () => {
               );
             })}
           </Slider>
+
+          <div className="slider-control d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+            <div className="slider-control-dots col-md-4 d-flex align-items-center"></div>
+            <div className="slider-control-buttons col-md-4 justify-content-end list-unstyled d-flex">
+              <button className="slidingButtons " onClick={previous}>
+                <ion-icon name="arrow-back-outline"></ion-icon>
+              </button>
+              <button className="slidingButtons " onClick={next}>
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </button>
+            </div>
+          </div>
+
+          {/* <h2>React Slick with CSS Modules</h2>
+          <div>
+            <Slider {...settingsWithModules}>
+              <div>
+                <p>Lorem Ipsum 1</p>
+              </div>
+              <div>
+                <p>Lorem Ipsum 2</p>
+              </div>
+              <div>
+                <p>Lorem Ipsum 3</p>
+              </div>
+            </Slider>
+          </div> */}
         </div>
 
         {/* <Slider {...settings}>
